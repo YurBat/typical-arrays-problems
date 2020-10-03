790c74f0b03411812a8d69c9d57e5c1316e369cb
YurBat <47013577+YurBat@users.noreply.github.com>
2020-10-03 19:13:14 +0300

    fix: Typical Arrays Problems task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
+1
 exports.min = function min (array) {
+  if (array === undefined || array == !-Infinity) {
   return 0;
+}
+else {
+  return Math.min(...array);
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array == !-Infinity) {
+    return 0;
+  }
+  else if (array != undefined) {
+  return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array == !-Infinity) {
+    return 0;
+  }
+  else {
+    return array.reduce((a, b) => (a + b)) / array.length;;
+  }
 }
+
+
+/*
+You should implement 3 functions: max, min, and avg for searching minimal value, maximal value, and an average value of a given array.
+
+For example:
+
+min(1, 2, 3, 4) // returns 1;
+max(1, 2, 3, 4) // returns 4;
+avg(1, 2, 3, 4) // returns 2.5;
+Please note, that you should return 0 if no params specified or an empty array passed 
+*/
